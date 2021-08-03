9189cce0379e084ecec24243c4ac44d3628f87ff
huyueqian <huyq@many-it.com>
2021-08-03 18:16:03 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/vue3-essential", "@vue/standard"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: [0, "double"],
+    semi: [1, "always"], //语句强制分号结尾
+    disallowTabs: false
   }
-}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title ">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe603;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe6e1;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe63c;</div>
+      <div class="docker__title">我的</div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  &__item {
+    display: inline-block;
+    text-align: center;
+    .iconfont {
+      font-size: 0.18rem;
+      margin: 0.07rem 0 0.02rem 0;
+    }
+  }
+  &__title {
+    font-size: 0.2rem;
+    /*浏览器能显示的最小字体是12px，可以用scale来缩放到10px*/
+    transform: scale(0.5);
+    transform-origin: center top;
+    &--active {
+      color: #1fa4fc;
     }
   }
 }
