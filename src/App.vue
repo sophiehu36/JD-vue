566ad77f1321d1be3d080a52c4259b10f9c5df04
huyueqian <huyq@many-it.com>
2021-08-05 15:30:20 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,23 @@
   <div class="wrapper">
     <div class="position">
       <span class="iconfont position__icon">&#xe631;</span>
-      <span class="position__text">北京理工大学国防科技园2号楼10层</span>
-      <span class="iconfont position__icon">&#xe628;</span>
+      北京理工大学国防科技园2号楼10层北京理工大学国防科技园2号楼10层
+      <span class="iconfont position__notice">&#xe628;</span>
     </div>
+    <div class="search">
+      <span class="iconfont">&#xe602;</span>
+      山姆会员商店优惠商品
+    </div>
+    <div class="banner">
+      <img
+        class="banner__img"
+        src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+        alt="banner"
+      />
+    </div>
+    <div>123123</div>
   </div>
+
   <div class="docker">
     <div class="docker__item docker__item--active">
       <div class="iconfont">&#xe600;</div>
@@ -27,6 +40,9 @@
 </template>
 
 <style lang="scss">
+@import "./style/variables.scss";
+@import "./style/mixins.scss";
+
 .wrapper {
   position: absolute;
   left: 0;
@@ -37,17 +53,48 @@
 }
 
 .position {
-  padding: 0.16rem 0;
+  position: relative;
+  padding: 0.16rem 0.24rem 0.16rem 0;
   font-size: 0.16rem;
-  display: flex;
-  align-items: baseline;
+  line-height: 0.22rem;
+  color: $content-fontcolor;
+  @include ellipsis;
   .position__icon {
     font-size: 0.2rem;
-    display: flex;
+    position: relative;
+    top: 0.015rem;
   }
-  &__text {
-    margin: 0 0.4rem 0 0.085rem;
-    text-overflow: ellipsis;
+  .position__notice {
+    font-size: 0.2rem;
+    position: absolute;
+    right: 0rem;
+    top: 0.175rem;
+  }
+}
+
+.search {
+  display: flex;
+  align-items: center;
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  font-size: 0.14rem;
+  color: #b7b7b7;
+  border-radius: 0.16rem;
+  margin-bottom: 0.12rem;
+  .iconfont {
+    font-size: 0.16rem;
+    padding: 0 0.12rem 0 0.16rem;
+  }
+}
+
+.banner {
+  // 撑开页面，防止外链图片加载导致页面抖动
+  height: 0;
+  overflow: hidden;
+  // 宽度的25.4%
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
   }
 }
 
@@ -61,6 +108,7 @@
   width: 100%;
   height: 0.49rem;
   border-top: 0.01rem solid #f1f1f1;
+  color: $content-fontcolor;
   &__item {
     display: inline-block;
     text-align: center;
